f302853a0b763585efcd6fb9f94dcd70f42cddb7
Graham Sutherland <gsuberland@gmail.com>
2020-02-13 17:34:39 +0000

    Added benchmarking.

---
+++ benchmark.bat
@@ -0,0 +1,23 @@
+.\BitPositionLookup\bin\Release\BitPositionLookup.exe > .\results_net48_anycpu_noverify.txt
+.\BitPositionLookup\bin\x86\Release\BitPositionLookup.exe > .\results_net48_x86_noverify.txt
+.\BitPositionLookup\bin\x64\Release\BitPositionLookup.exe > .\results_net48_x64_noverify.txt
+
+.\BitPositionLookup\bin\Release_VERIFY_COUNTS\BitPositionLookup.exe > .\results_net48_anycpu_verify_counts.txt
+.\BitPositionLookup\bin\x86\Release_VERIFY_COUNTS\BitPositionLookup.exe > .\results_net48_x86_verify_counts.txt
+.\BitPositionLookup\bin\x64\Release_VERIFY_COUNTS\BitPositionLookup.exe > .\results_net48_x64_verify_counts.txt
+
+.\BitPositionLookup\bin\Release_VERIFY_FIRST\BitPositionLookup.exe > .\results_net48_anycpu_verify_first.txt
+.\BitPositionLookup\bin\x86\Release_VERIFY_FIRST\BitPositionLookup.exe > .\results_net48_x86_verify_first.txt
+.\BitPositionLookup\bin\x64\Release_VERIFY_FIRST\BitPositionLookup.exe > .\results_net48_x64_verify_first.txt
+
+.\BitPositionLookupCore\bin\Release\netcoreapp3.1\BitPositionLookupCore.exe > .\results_core31_anycpu_noverify.txt
+.\BitPositionLookupCore\bin\x86\Release\netcoreapp3.1\BitPositionLookupCore.exe > .\results_core31_x86_noverify.txt
+.\BitPositionLookupCore\bin\x64\Release\netcoreapp3.1\BitPositionLookupCore.exe > .\results_core31_x64_noverify.txt
+
+.\BitPositionLookupCore\bin\Release_VERIFY_COUNTS\netcoreapp3.1\BitPositionLookupCore.exe > .\results_core31_anycpu_verify_counts.txt
+.\BitPositionLookupCore\bin\x86\Release_VERIFY_COUNTS\netcoreapp3.1\BitPositionLookupCore.exe > .\results_core31_x86_verify_counts.txt
+.\BitPositionLookupCore\bin\x64\Release_VERIFY_COUNTS\netcoreapp3.1\BitPositionLookupCore.exe > .\results_core31_x64_verify_counts.txt
+
+.\BitPositionLookupCore\bin\Release_VERIFY_FIRST\netcoreapp3.1\BitPositionLookupCore.exe > .\results_core31_anycpu_verify_first.txt
+.\BitPositionLookupCore\bin\x86\Release_VERIFY_FIRST\netcoreapp3.1\BitPositionLookupCore.exe > .\results_core31_x86_verify_first.txt
+.\BitPositionLookupCore\bin\x64\Release_VERIFY_FIRST\netcoreapp3.1\BitPositionLookupCore.exe > .\results_core31_x64_verify_first.txt
