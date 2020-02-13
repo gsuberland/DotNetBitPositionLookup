5e2695fb7b6b55c1938b4177011fda364c79196f
Graham Sutherland <gsuberland@gmail.com>
2020-02-13 16:40:36 +0000

    Added .NET Core variant.

---
+++ BitPositionLookupCore/Benchmark.cs
@@ -0,0 +1,425 @@
+﻿// uncomment this if you want to verify that the set bit counts match up across lookup/calculate.
+//#define VERIFY_COUNTS
+// uncomment this if you want to verify that the first bit indices match up across lookup/calculate.
+//#define VERIFY_FIRST
+// ^ setting either of these can skew the benchmarks a bit since you're adding a .Count() or .FirstOrDefault() call into each check.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+	// specify seed for reproducibility
+	static Random rng = new Random(0x5E7B175);
+
+	static byte[] bytePool = null;
+	static sbyte[] sbytePool = null;
+	static ushort[] ushortPool = null;
+	static short[] shortPool = null;
+	static uint[] uintPool = null;
+	static int[] intPool = null;
+	static ulong[] ulongPool = null;
+	static long[] longPool = null;
+
+	static void Main()
+	{
+		Console.WriteLine("Forcing table generation before benchmark...");
+		var sw = new Stopwatch();
+
+		sw.Start();
+		var result8 = SetBitPositionLookup.Lookup((byte)53);
+		sw.Stop();
+		Console.WriteLine("(byte)53  = [ " + string.Join(", ", result8) + " ]");
+		Console.WriteLine("8-bit LUT generation took {0:0.0000}ms", sw.Elapsed.TotalMilliseconds);
+		sw.Reset();
+
+		sw.Start();
+		var result16 = SetBitPositionLookup.Lookup((UInt16)53);
+		sw.Stop();
+		Console.WriteLine("(ushort)53 = [ " + string.Join(", ", result16) + " ]");
+		Console.WriteLine("16-bit LUT generation took {0:0.0000}ms", sw.Elapsed.TotalMilliseconds);
+		sw.Reset();
+
+		Console.WriteLine();
+		Console.WriteLine("Beginning benchmarks.");
+
+		int[] testSizes = { 1024, 8 * 1024, 64 * 1024, 256 * 1024, 512 * 1024, 1024 * 1024, 4 * 1024 * 1024 };
+
+		foreach (int testSize in testSizes)
+		{
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("Testing {0}K elements:", testSize / 1024);
+
+			InitPools(testSize);
+			FillPools(testSize);
+
+			// there are 16 tests (8 types, lookup and calc each)
+			int[] results = new int[16];
+			int testNum = 0;
+
+			Console.WriteLine("+--------+------------+------------+");
+			Console.WriteLine("| Type   | Lookup     | Calculate  |");
+			Console.WriteLine("+--------+------------+------------+");
+
+			// benchmark byte
+			(results[testNum++], results[testNum++]) = Benchmark(
+				"byte",
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Lookup(bytePool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				},
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Calculate(bytePool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				});
+
+			// benchmark sbyte
+			(results[testNum++], results[testNum++]) = Benchmark(
+				"sbyte",
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Lookup(sbytePool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				},
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Calculate(sbytePool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				});
+
+			// benchmark uint16
+			(results[testNum++], results[testNum++]) = Benchmark(
+				"ushort",
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Lookup(ushortPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				},
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Calculate(ushortPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				});
+
+			// benchmark int16
+			(results[testNum++], results[testNum++]) = Benchmark(
+				"short",
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Lookup(shortPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				},
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Calculate(shortPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				});
+
+			// benchmark int32
+			(results[testNum++], results[testNum++]) = Benchmark(
+				"int",
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Lookup(intPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				},
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Calculate(intPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				});
+
+			// benchmark uint32
+			(results[testNum++], results[testNum++]) = Benchmark(
+				"uint",
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Lookup(uintPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				},
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Calculate(uintPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				});
+
+			// benchmark int64
+			(results[testNum++], results[testNum++]) = Benchmark(
+				"long",
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Lookup(longPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				},
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Calculate(longPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				});
+
+			// benchmark uint64
+			(results[testNum++], results[testNum++]) = Benchmark(
+				"ulong",
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Lookup(ulongPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				},
+				() =>
+				{
+					int r = 0;
+					for (int n = 0; n < testSize; n++)
+					{
+						var luts = SetBitPositionLookup.Calculate(ulongPool[n]);
+#if VERIFY_COUNTS
+					r += luts.Count();
+#elif VERIFY_FIRST
+					r += luts.FirstOrDefault();
+#endif
+					}
+					return r;
+				});
+
+			Console.WriteLine("+--------+------------+------------+");
+			Console.WriteLine();
+
+#if VERIFY_COUNTS || VERIFY_FIRST
+		for (int i = 0; i < 8; i++)
+		{
+			int lookupResult = results[i * 2];
+			int calcResult = results[i * 2 + 1];
+			Console.WriteLine("Test {0}: {1}", i + 1, (lookupResult == calcResult) ? "PASS" : string.Format("FAIL ({0} != {1})", lookupResult, calcResult));
+		}
+#endif
+		}
+	}
+
+	static (int, int) Benchmark(string typeName, Func<int> lookupFunc, Func<int> calcFunc)
+	{
+		var sw = new Stopwatch();
+		int lookupResult = 0;
+		int calcResult = 0;
+		Console.Write("| " + typeName.PadRight(6) + " | ");
+		sw.Start();
+		lookupResult = lookupFunc();
+		sw.Stop();
+		Console.Write(string.Format("{0:0.000}", sw.Elapsed.TotalMilliseconds).PadRight(10) + " | ");
+		sw.Reset();
+		sw.Start();
+		calcResult = calcFunc();
+		sw.Stop();
+		Console.WriteLine(string.Format("{0:0.000}", sw.Elapsed.TotalMilliseconds).PadRight(10) + " |");
+		sw.Reset();
+		return (lookupResult, calcResult);
+	}
+
+	static void InitPools(int testSize)
+	{
+		bytePool = new byte[testSize];
+		sbytePool = new sbyte[testSize];
+		ushortPool = new ushort[testSize];
+		shortPool = new short[testSize];
+		uintPool = new uint[testSize];
+		intPool = new int[testSize];
+		ulongPool = new ulong[testSize];
+		longPool = new long[testSize];
+	}
+
+	static void FillPools(int testSize)
+	{
+		Console.Write("Filling pools.");
+
+		// fill byte
+		rng.NextBytes(bytePool);
+		Console.Write(".");
+		// fill sbyte
+		for (int i = 0; i < testSize; i++)
+		{
+			sbytePool[i] = checked((sbyte)rng.Next(sbyte.MinValue, (int)sbyte.MaxValue + 1));
+		}
+		Console.Write(".");
+		// fill ushort
+		for (int i = 0; i < testSize; i++)
+		{
+			ushortPool[i] = checked((ushort)rng.Next(ushort.MinValue, (int)ushort.MaxValue + 1));
+		}
+		Console.Write(".");
+		// fill short
+		for (int i = 0; i < testSize; i++)
+		{
+			shortPool[i] = checked((short)rng.Next(short.MinValue, (int)short.MaxValue + 1));
+		}
+		Console.Write(".");
+		// fill uint
+		byte[] intBuf = new byte[4];
+		for (int i = 0; i < testSize; i++)
+		{
+			rng.NextBytes(intBuf);
+			uintPool[i] = BitConverter.ToUInt32(intBuf, 0);
+		}
+		Console.Write(".");
+		// fill int
+		for (int i = 0; i < testSize; i++)
+		{
+			rng.NextBytes(intBuf);
+			intPool[i] = BitConverter.ToInt32(intBuf, 0);
+		}
+		Console.Write(".");
+		// fill ulong
+		byte[] longBuf = new byte[8];
+		for (int i = 0; i < testSize; i++)
+		{
+			rng.NextBytes(longBuf);
+			ulongPool[i] = BitConverter.ToUInt64(longBuf, 0);
+		}
+		Console.Write(".");
+		// fill long
+		for (int i = 0; i < testSize; i++)
+		{
+			rng.NextBytes(intBuf);
+			longPool[i] = BitConverter.ToInt64(longBuf, 0);
+		}
+		Console.Write(".");
+		Console.WriteLine();
+	}
+}
---
+++ BitPositionLookupCore/SetBitPositionLookup.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+
+public static class SetBitPositionLookup
+{
+	private static readonly Lazy<byte[]> BitPositionLookup8;
+	private static readonly Lazy<byte[]> BitPositionLookup16;
+
+	static SetBitPositionLookup()
+	{
+		BitPositionLookup8 = new Lazy<byte[]>(() => { return GenerateLookupTable(8); });
+		BitPositionLookup16 = new Lazy<byte[]>(() => { return GenerateLookupTable(16); });
+	}
+
+	private static byte[] GenerateLookupTable(int valueSize)
+	{
+		// this function generates a set bit position lookup table for n-bit chunks of an integer.
+		// each group of n elements in the array represents contains the indices of set bits for an input value.
+		// for example, the number 6 (00000110) would be encoded as {1,2,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF} as the 1st and 2nd bits are set.
+
+		// only allow 8-bit and 16-bit lookups (LUT sizes of 2KB / 1MB)
+		// you can technically do 32-bit lookups but the LUT would be 128GB in size (!!!) and would take a long time to generate.
+		if (valueSize != 8 && valueSize != 16)
+		{
+			throw new ArgumentOutOfRangeException(nameof(valueSize), "Value size must be 8 or 16.");
+		}
+
+		// calculate the number of values in an n-bit integer
+		int valueCount = 1 << valueSize;
+		// calculate the size of the table necessary to hold all the values
+		int tableSize = valueCount * valueSize;
+
+		byte[] lut = new byte[tableSize];
+
+		// initially set all LUT elements to a padding value (0xFF)
+		for (int i = 0; i < lut.Length; i++)
+		{
+			lut[i] = 0xFF;
+		}
+
+		// loop through all possible input values (0 .. 2^n-1)
+		for (int i = 0; i < valueCount; i++)
+		{
+			// calculate table pointer for this input value (i * n)
+			int tablePtr = i * valueSize;
+			// loop through all the bit positions (0 .. n-1)
+			for (int b = 0; b < valueSize; b++)
+			{
+				// if the b'th bit is set, add this bit position to the table
+				if ((i & (1 << b)) != 0)
+				{
+					lut[tablePtr++] = (byte)b;
+				}
+			}
+		}
+		return lut;
+	}
+
+	public static int[] Lookup(byte value)
+	{
+		if (value == 0)
+			return new int[] { };
+
+		// get the 8-bit LUT
+		var lut = BitPositionLookup8.Value;
+
+		// calcualte the offset of this value in the LUT
+		int offset = value * 8;
+
+		// find out how many set bits we have
+		int count = 0;
+		for (int n = 0; n < 8; n++)
+		{
+			if (lut[offset + n] != 0xFF)
+				count++;
+		}
+
+		// extract the bit positions
+		int[] positions = new int[count];
+		for (int i = 0; i < count; i++)
+		{
+			positions[i] = (int)lut[offset + i];
+		}
+		return positions;
+	}
+
+	public static int[] Lookup(UInt16 value)
+	{
+		if (value == 0)
+			return new int[] { };
+
+		// get the 16-bit LUT
+		var lut = BitPositionLookup16.Value;
+
+		// calcualte the offset of this value in the LUT
+		int offset = value * 16;
+
+		// find out how many set bits we have
+		int count = 0;
+		for (int n = 0; n < 16; n++)
+		{
+			if (lut[offset + n] != 0xFF)
+				count++;
+		}
+
+		// extract the bit positions
+		int[] positions = new int[count];
+		for (int i = 0; i < count; i++)
+		{
+			positions[i] = (int)lut[offset + i];
+		}
+		return positions;
+	}
+
+	public static IEnumerable<int> Lookup(UInt32 value)
+	{
+		if (value == 0)
+			return new int[] { };
+		// use the popcnt intrinsic to find the number of set bits for the input value
+		uint setBitCount = Popcnt.PopCount(value);
+		// we now know how many results we will have, so we can use a fixed array size
+		int[] results = new int[setBitCount];
+
+		UInt16 a = unchecked((UInt16)(value & 0xFFFFU));
+		UInt16 b = unchecked((UInt16)((value >> 16) & 0xFFFFU));
+
+		var lowPositions = Lookup(a); // get positions for lower 16-bit chunk
+		Array.Copy(lowPositions, 0, results, 0, lowPositions.Length);
+
+		if (b != 0)
+		{
+			var highPositions = Lookup(b); // get positions for upper 16-bit chunk
+			int offset = lowPositions.Length;
+			for (int i = 0; i < highPositions.Length; i++)
+			{
+				results[offset + i] = highPositions[i] + 16; // offset indices by 16
+			}
+		}
+
+		return results;
+	}
+
+	public static IEnumerable<int> Lookup(UInt64 value)
+	{
+		if (value == 0)
+			return new int[] { };
+
+		// use the popcnt intrinsic to find the number of set bits for the input value
+		ulong setBitCount = Popcnt.X64.PopCount(value);
+		// we now know how many results we will have, so we can use a fixed array size
+		int[] results = new int[setBitCount];
+
+		UInt16 a = unchecked((UInt16)(value & 0xFFFFU));
+		UInt16 b = unchecked((UInt16)((value >> 16) & 0xFFFFU));
+		UInt16 c = unchecked((UInt16)((value >> 32) & 0xFFFFU));
+		UInt16 d = unchecked((UInt16)((value >> 48) & 0xFFFFU));
+
+		var lowPositions = Lookup(a); // get positions for lower 16-bit chunk
+		Array.Copy(lowPositions, 0, results, 0, lowPositions.Length);
+
+		int offset = lowPositions.Length;
+
+		if (b != 0)
+		{
+			var bitPositions = Lookup(b); // get positions for mid-low 16-bit chunk
+			for (int i = 0; i < bitPositions.Length; i++)
+			{
+				results[offset + i] = bitPositions[i] + 16; // offset indices by 16
+			}
+			offset += bitPositions.Length;
+		}
+
+		if (c != 0)
+		{
+			var bitPositions = Lookup(c); // get positions for mid-high 16-bit chunk
+			for (int i = 0; i < bitPositions.Length; i++)
+			{
+				results[offset + i] = bitPositions[i] + 32; // offset indices by 32
+			}
+			offset += bitPositions.Length;
+		}
+
+		if (d != 0)
+		{
+			var bitPositions = Lookup(d); // get positions for high 16-bit chunk
+			for (int i = 0; i < bitPositions.Length; i++)
+			{
+				results[offset + i] = bitPositions[i] + 48; // offset indices by 48
+			}
+		}
+
+		return results;
+	}
+
+	public static IEnumerable<int> Lookup(sbyte value) => Lookup(unchecked((byte)value));
+
+	public static IEnumerable<int> Lookup(Int16 value) => Lookup(unchecked((UInt16)value));
+
+	public static IEnumerable<int> Lookup(Int32 value) => Lookup(unchecked((UInt32)value));
+
+	public static IEnumerable<int> Lookup(Int64 value) => Lookup(unchecked((UInt64)value));
+
+	public static IEnumerable<int> Calculate(byte value)
+	{
+		const int TypeSize = 8;
+		var result = new List<int>(TypeSize);
+		for (int n = 0; n < TypeSize; n++)
+		{
+			if ((value & (1U << n)) != 0)
+			{
+				result.Add(n);
+			}
+		}
+		return result;
+	}
+
+	public static IEnumerable<int> Calculate(UInt16 value)
+	{
+		const int TypeSize = 16;
+		var result = new List<int>(TypeSize);
+		for (int n = 0; n < TypeSize; n++)
+		{
+			if ((value & (1U << n)) != 0)
+			{
+				result.Add(n);
+			}
+		}
+		return result;
+	}
+
+	public static IEnumerable<int> Calculate(UInt32 value)
+	{
+		const int TypeSize = 32;
+		var result = new List<int>(TypeSize);
+		for (int n = 0; n < TypeSize; n++)
+		{
+			if ((value & (1U << n)) != 0)
+			{
+				result.Add(n);
+			}
+		}
+		return result;
+	}
+
+	public static IEnumerable<int> Calculate(UInt64 value)
+	{
+		const int TypeSize = 64;
+		var result = new List<int>(TypeSize);
+		for (int n = 0; n < TypeSize; n++)
+		{
+			if ((value & (1UL << n)) != 0)
+			{
+				result.Add(n);
+			}
+		}
+		return result;
+	}
+
+	public static IEnumerable<int> Calculate(sbyte value) => Calculate(unchecked((byte)value));
+
+	public static IEnumerable<int> Calculate(Int16 value) => Calculate(unchecked((UInt16)value));
+
+	public static IEnumerable<int> Calculate(Int32 value) => Calculate(unchecked((UInt32)value));
+
+	public static IEnumerable<int> Calculate(Int64 value) => Calculate(unchecked((UInt64)value));
+}
